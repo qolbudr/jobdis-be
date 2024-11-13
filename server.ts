9d9b77d99ad8011535da182aa93b1cb47e0a43fe
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2024-11-13 20:50:14 +0700

    fix: revert port to 3000

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import * as fs from 'fs';
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 4000;
+const port = 3000;
 
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
